0f6e6fa3e5356e4e15d4c86173edb95f36872b43
MLB-78 <100378645+MLB-78@users.noreply.github.com>
2024-05-23 21:44:09 +0200

    return

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import LoadingSpinner from './components/homepage/js/LoadingSpinner';
@@ -7,6 +6,8 @@ import CoverSection from './components/homepage/js/CoverSection';
 import Info from './components/homepage/js/Info';
 import Cards from './components/homepage/js/Cards';
 import Footer from './components/homepage/js/Footer';
+// import Fighters from './components/fighters/Fighters';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -20,19 +21,30 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      {loading ? (
-        <LoadingSpinner />
-      ) : (
-        <>
-          <Navbar />
-          <CoverSection />
-          <Info />
-          <Cards />
-          <Footer />
-        </>
-      )}
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        {loading ? (
+          <LoadingSpinner />
+        ) : (
+          <Routes>
+            <Route path="/" element={
+              <div className="homepage">
+                <CoverSection />
+                <Info />
+                <Cards />
+              </div>
+            } />
+            <Route path="/Fighters" element={
+              <div className="fighters">
+                <Fighters />
+              </div>
+            } />
+          </Routes>
+        )}
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/fighters/fighters.js
+++ src/components/fighters/fighters.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './Fighters.css'; // Assurez-vous de créer un fichier CSS pour le style
 
-function fighters() {
-  return (
-    <h1>KOTO AMATSU KAMI</h1>
-  )
+function Fighters() {
+  <div class="dr">aweeee</div>
+  
+  // const [fighters, setFighters] = useState([]);
+
+  // useEffect(() => {
+  //   axios.get('http://localhost:5000/api/fighters')
+  //     .then(response => {
+  //       setFighters(response.data);
+  //     })
+  //     .catch(error => {
+  //       console.error('There was an error fetching the fighters!', error);
+  //     });
+  // }, []);
+
+  // return (
+  //   <div>
+  //     <h1>Les meilleurs fighters du moment</h1>
+  //     <div className="fighters-container">
+  //       {fighters.map(fighter => (
+  //         <div key={fighter._id} className="fighter-card">
+  //           <div className="left-section">
+  //             <h2 className="text-black">{fighter.nom}</h2>
+  //             <img src={fighter.image} alt={fighter.nom} className="fighter-image" />
+  //             <p className="text-black">Âge : {fighter.âge}</p>
+  //             <img src={fighter.flag} alt="flag" className="flag-image" />
+  //           </div>
+  //           <div className="right-section">
+  //             <p className="text-black">{fighter.description}</p>
+  //             <h1 className="text-black">Highlight</h1>
+  //             <iframe
+  //               className="video-frame"
+  //               width="560"
+  //               height="315"
+  //               src={fighter.video}
+  //               title="YouTube video player"
+  //               frameBorder="0"
+  //               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  //               allowFullScreen
+  //             ></iframe>
+  //           </div>
+  //         </div>
+  //       ))}
+  //     </div>
+  //   </div>
+  // );
 }
 
-export default fighters
+export default Fighters;
